f80bf8ea24963b62183c5d9d757ac09c0b8da8f9
henryqin <henryqin@umich.edu>
2021-06-27 22:16:59 -0700

    midtermmap

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public static CameraController instance;
+
+	public float moveSpeed = 30;
+
+	public Transform target;
+
+    private void Awake(){
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null){
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeTarget(Transform newTarget){
+        target = newTarget;
+    }
+}
