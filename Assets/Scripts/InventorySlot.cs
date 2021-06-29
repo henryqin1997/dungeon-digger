6641f9ff6fff603392afe557a8632dc6dc518325
wrpaape <wrpaape@gmail.com>
2021-06-28 20:35:57 -0400

    Add event for dish consumption: Inventory.dishConsumedEvent(Dish)

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
+    void Start()
+    {
+    }
+
     // Update is called once per frame
     public void SetItems(IItem item, int count)
     {
         SetFrame(item.GetFrame());
         SetIcon( item.GetIcon());
         SetCount(count);
+    }
+
+    public void SetSelectable(bool selectable)
+    {
+        GetButton().interactable = selectable;
+    }
+
+    public void SetSelectCallback(UnityAction call)
+    {
+        UnityEvent onClick = GetButton().onClick;
+        onClick.RemoveAllListeners();
+        onClick.AddListener(call);
+    }
+
+    private Button GetButton()
+    {
+        return gameObject.GetComponent<Button>();
     }
 
     private void SetFrame(Sprite frame)
@@ -31,8 +53,13 @@ public class InventorySlot : MonoBehaviour
 
     private T GetDescendantComponent<T>(string path)
     {
+        return GetDescendant(path).GetComponent<T>();
+    }
+
+    private GameObject GetDescendant(string path)
+    {
         Transform transform = gameObject.transform.Find(path);
         Debug.Assert(transform != null);
-        return transform.gameObject.GetComponent<T>();
+        return transform.gameObject;
     }
 }
