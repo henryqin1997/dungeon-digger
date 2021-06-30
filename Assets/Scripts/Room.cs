4f8b472babe2af0841b6e2775ba4f6172a0a4b61
Dragon-Abyss <markzhao16@gmail.com>
2021-06-30 15:07:45 -0700

    rooms tracker fix

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -12,6 +12,13 @@ public class Room : MonoBehaviour
 
     private bool roomActive;
 
+    private static bool room_entered = false;
+    private static bool room1_entered = false;
+    private static bool room2_entered = false;
+    private static bool room3_entered = false;
+    private static bool room4_entered = false;
+    private static bool room5_entered = false;
+    private static bool room6_entered = false;
 
     // Start is called before the first frame update
     void Start()
@@ -55,6 +62,48 @@ public class Room : MonoBehaviour
         {
             CameraController.instance.ChangeTarget(transform);
             RoomsEnteredTracker.rooms_entered++;
+            if (gameObject.name == "Room") {
+                if (!room_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room_entered = true;
+            }
+            if (gameObject.name == "Room (1)") {
+                if (!room1_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room1_entered = true;
+            }
+            if (gameObject.name == "Room (2)") {
+                if (!room2_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room2_entered = true;
+            }
+            if (gameObject.name == "Room (3)") {
+                if (!room3_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room3_entered = true;
+            }
+            if (gameObject.name == "Room (4)") {
+                if (!room4_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room4_entered = true;
+            }
+            if (gameObject.name == "Room (5)") {
+                if (!room5_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room5_entered = true;
+            }
+            if (gameObject.name == "Room (6)") {
+                if (!room6_entered) {
+                    RoomsEnteredTracker.unique_rooms_entered++;
+                }
+                room6_entered = true;
+            }
             if (closeWhenEntered)
             {
                 foreach (GameObject door in doors)
--- Assets/Scripts/RoomsEnteredTracker.cs
+++ Assets/Scripts/RoomsEnteredTracker.cs
@@ -6,6 +6,7 @@ using UnityEngine.Analytics;
 public class RoomsEnteredTracker : MonoBehaviour
 {
     public static int rooms_entered = 0;
+    public static int unique_rooms_entered = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,16 @@ public class RoomsEnteredTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log("rooms" + rooms_entered);
+        Debug.Log("unique" + unique_rooms_entered);
     }
 
     public static void SendRoomsEntered()
     {
-        Analytics.CustomEvent("Rooms Entered Before Dead");
+        Analytics.CustomEvent("roomsEntered", new Dictionary<string, object>
+        {
+            { "totalRoomsEntered", rooms_entered },
+            { "uniqueRoomsEntered", unique_rooms_entered }
+        });
     }
 }
