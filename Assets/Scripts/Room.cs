dbe607b5197de061e283b566e2f3f572d47d7c35
henryqin <henryqin@umich.edu>
2021-07-18 22:17:22 -0700

    random level

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -13,13 +13,31 @@ public class Room : MonoBehaviour
 
     private bool roomActive;
 
-    public UnityEvent<GameObject> roomEnteredEvent = new UnityEvent<GameObject>();
+    // public UnityEvent<GameObject> roomEnteredEvent = new UnityEvent<GameObject>();
 
     public string roomtype = "normal";
 
     // Start is called before the first frame update
     void Start()
     {   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roomActive && closeWhenEntered && (enemies.Count == 0))
+        {
+            foreach (GameObject door in doors)
+            {
+                door.SetActive(false);
+            }
+            closeWhenEntered = false;
+        }
+    }
+
+    public void setup()
+    {
         if (roomtype.Equals("normal")){
             int enemy_count = (int) Random.Range(2,6);
             GameObject[] enemylist = Resources.LoadAll<GameObject>("Prefabs/Enemy");
@@ -44,24 +62,14 @@ public class Room : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (roomActive && closeWhenEntered && (enemies.Count == 0))
-        {
-            foreach (GameObject door in doors)
-            {
-                door.SetActive(false);
-            }
-            closeWhenEntered = false;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            roomEnteredEvent.Invoke(gameObject);
+            // roomEnteredEvent.Invoke(gameObject);
+            GameObject MainCamera = GameObject.Find("Main Camera");
+            CameraController cc = MainCamera.GetComponent<CameraController>();
+            cc.ChangeTarget(this.transform);
 
             if (closeWhenEntered)
             {
---
+++ Assets/Scripts/level_generator.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level_generator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public GameObject MainCamera;
+
+    void Start()
+    {
+        generate_level(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void generate_level(int level)
+    {
+    	//Legal input level: 1, 2, 3
+
+    	player.transform.position = new Vector3(0, 0, player.transform.position.z);
+    	MainCamera.transform.position = new Vector3(0, 0, -10);
+
+    	List<RoomConfig> layout = RoomLayoutGenerator.GenerateRoomLayout(5);
+
+    	foreach (RoomConfig rc in layout)
+    	{
+    		string room_postfix = "";
+    		if (((int)rc.doors & 1)>0){
+    			room_postfix += "l";
+    		}
+    		if (((int)rc.doors & 2)>0){
+    			room_postfix += "r";
+    		}
+    		if (((int)rc.doors & 4)>0){
+    			room_postfix += "u";
+    		}
+    		if (((int)rc.doors & 8)>0){
+    			room_postfix += "d";
+    		}
+    		GameObject temproom = Resources.Load("Prefabs/Room/DoorRooms/Room_"+room_postfix) as GameObject;
+    		temproom = Instantiate(temproom);
+    		temproom.transform.position = new Vector3(28.8f*rc.position.x, 16f*rc.position.y, 12.07283f);
+			temproom.GetComponent<Room>().roomtype = rc.type;
+    		if (rc.type == "initial")
+    		{
+    			temproom.GetComponent<Room>().closeWhenEntered = false;
+    			temproom.GetComponent<Room>().openWhenEnemiesCleared = true;
+    			
+    			if (level == 1){
+    				GameObject initial = Resources.Load("Prefabs/Room/Intro Display") as GameObject;
+    				initial = Instantiate(initial) as GameObject;
+    				initial.transform.position = new Vector3(-0.4f,1.96f,0f);
+    				initial.transform.parent = temproom.transform;
+    			}
+    		}
+    		else if (rc.type == "boss")
+    		{
+    			temproom.GetComponent<Room>().closeWhenEntered = true;
+    			temproom.GetComponent<Room>().openWhenEnemiesCleared = true;
+    			GameObject boss = Resources.Load("Prefabs/boss_prefab/Boss System") as GameObject;
+    			boss = Instantiate(boss);
+    			boss.transform.position = new Vector3(28.8f*rc.position.x, 16f*rc.position.y, 12.07283f);
+    			boss.transform.parent = temproom.transform;
+    			temproom.GetComponent<Room>().enemies.Add(boss);
+    		}
+    		else
+    		{
+    			temproom.GetComponent<Room>().closeWhenEntered = true;
+    			temproom.GetComponent<Room>().openWhenEnemiesCleared = true;
+    		}
+    		Room tmp = temproom.GetComponent<Room>();
+    		tmp.setup();
+    	}
+
+    }
+}
